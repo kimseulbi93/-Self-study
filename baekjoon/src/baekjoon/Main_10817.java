bd98b8b01c149465ab6647e0006ff0da3fb6ab83
kimseulbi93 <kimseulbi93@users.noreply.github.com>
2020-03-20 11:12:06 +0900

    ok

--- baekjoon/src/baekjoon/Main_10817.java
+++ baekjoon/src/baekjoon/Main_10817.java
@@ -25,3 +25,30 @@ public class Main_10817 {
 }
 
 
+/*
+ * import java.io.*; import java.util.*;
+ * 
+ * public class Main{
+ * 
+ * public static void main(String[] args) throws IOException {
+ * 
+ * BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ * StringTokenizer st = new StringTokenizer(br.readLine());
+ * 
+ * int A = Integer.parseInt(st.nextToken()); int B =
+ * Integer.parseInt(st.nextToken()); int C = Integer.parseInt(st.nextToken());
+ * 
+ * if( 1 <= A && A<=100 && 1 <= B && B<=100 && 1 <=C && C<=100) { if(A<B) {
+ * if(B<C) { System.out.println(B);
+ * 
+ * }else if(A<C) { System.out.println(C);
+ * 
+ * }else { System.out.println(A); } }else { if (A<C) { System.out.println(A);
+ * }else if(C<B) { System.out.println(B);
+ * 
+ * }else { System.out.println(C); } }
+ * 
+ * }
+ * 
+ * } }
+ */
