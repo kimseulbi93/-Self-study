7de9aff4aa74e6d701d64dd6f1df13ef8ac3e0ab
kimseulbi93 <kimseulbi93@users.noreply.github.com>
2020-03-20 10:41:00 +0900

    ok

--- baekjoon/src/baekjoon/Test_5543.java
+++ baekjoon/src/baekjoon/Test_5543.java
@@ -1,10 +1,32 @@
 package baekjoon;
 
+import java.util.*;
+import java.io.*;
+
+
+
 public class Test_5543 {
 
-	public static void main(String[] args) {
-		System.out.println("Hello");
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int burger = Integer.MAX_VALUE;
+		for (int i = 0; i <3; i++) {
+			burger = Math.min(burger, Integer.parseInt(br.readLine()));
+			
+		}
+		int drink = Integer.MAX_VALUE;
+		for(int j = 0; j <2; j++) {
+			drink = Math.min(drink, Integer.parseInt(br.readLine()));
+		}
+		
+		bw.write(String.valueOf(burger+drink - 50));
+		
+		bw.flush();
 
+		bw.close();
 	}
 
 }
